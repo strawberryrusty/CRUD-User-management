450ea4262e67f74eebb733c0bd761d3610f20af8
Prashanth Mohan <prashmohanuon@gmail.com>
2022-06-28 14:47:49 +0200

    create user form data passing

--- src/app/user-create/user-create.component.ts
+++ src/app/user-create/user-create.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup,  } from '@angular/forms';
 
 @Component({
   selector: 'app-user-create',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-create.component.css']
 })
 export class UserCreateComponent implements OnInit {
+  newUserForm!: FormGroup;
 
-  constructor() { }
+  constructor( private fb: FormBuilder) { }
 
   ngOnInit(): void {
+
+    this.newUserForm = this.fb.group({
+    userId: Math.floor(1000 + Math.random() * 9000),
+    firstName: [''],
+    lastName: ['']
+  });
   }
+
+     onSubmit(){
+      console.log(this.newUserForm);
+    }
 
 }
